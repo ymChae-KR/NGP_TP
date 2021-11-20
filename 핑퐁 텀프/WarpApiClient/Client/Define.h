9cc6aaf302699b1be66ab07b39dfd9c341658193
Young Mun Chae <dudans1536@nate.com>
2021-11-20 23:34:54 +0900

    client public func update

--- 핑퐁 텀프/WarpApiClient/Client/Define.h
+++ 핑퐁 텀프/WarpApiClient/Client/Define.h
@@ -1,15 +1,4 @@
-/*
-	kpu-warp-winapi-framework		Copyright ⓒ https://github.com/KPU-WARP
-
-	#0. 해당 프로젝트는 한국산업기술대 게임공학과 학술 소모임 WARP의 오픈소스 WinAPI Framework입니다.
-	#1. 관련 세부 정보는 깃헙 저장소 리드미에서 확인하실 수 있습니다. 
-		- https://github.com/KPU-WARP/winapi-framework
-	
-	#2. 기타 문의는, KoreaGameMaker@gmail.com으로 부탁드립니다. 감사합니다 :)
-*/
-
 #pragma once
-
 #define MAIN_TIMER			1	
 #define MAIN_TIEMR_FRAME	17 // 60FPS
 
@@ -56,4 +45,22 @@ enum VK_KEY
 	VK_Y,
 	VK_Z
 };
-// KeyBoard
+
+typedef char s8;
+typedef unsigned char u8;
+typedef short s16;
+typedef unsigned short u16;
+typedef int s32;
+typedef unsigned int u32;
+typedef long long s64;
+typedef unsigned long long u64;
+
+#define global_variable static
+#define internal static
+
+inline int
+clamp(int min, int val, int max) {
+	if (val < min) return min;
+	if (val > max) return max;
+	return val;
+}
