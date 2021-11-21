fe436ab0a1106d1bbb9f7607c0fb127451fa9f09
Young Mun Chae <dudans1536@nate.com>
2021-11-21 17:03:27 +0900

    Client public func update 2

--- 핑퐁 텀프/WarpApiClient/Client/Client.cpp
+++ 핑퐁 텀프/WarpApiClient/Client/Client.cpp
@@ -1,14 +1,4 @@
-﻿/*
-	kpu-warp-winapi-framework		Copyright ⓒ https://github.com/KPU-WARP
-
-	#0. 해당 프로젝트는 한국산업기술대 게임공학과 학술 소모임 WARP의 오픈소스 WinAPI Framework입니다.
-	#1. 관련 세부 정보는 깃헙 저장소 리드미에서 확인하실 수 있습니다.
-		- https://github.com/KPU-WARP/winapi-framework
-
-	#2. 기타 문의는, KoreaGameMaker@gmail.com으로 부탁드립니다. 감사합니다 :)
-*/
-
-#include "stdafx.h"
+﻿#include "stdafx.h"
 #include "Client.h"
 #include "GameFramework.h"
 
@@ -18,7 +8,9 @@
 HINSTANCE hInst;                                // 현재 인스턴스입니다.
 WCHAR szTitle[MAX_LOADSTRING];                  // 제목 표시줄 텍스트입니다.
 WCHAR szWindowClass[MAX_LOADSTRING];            // 기본 창 클래스 이름입니다.
-static WGameFramework gGameFramework;
+
+global_variable WGameFramework gGameFramework;
+global_variable Render_State render_state;
 
 // 이 코드 모듈에 포함된 함수의 선언을 전달합니다:
 ATOM                MyRegisterClass(HINSTANCE hInstance);
